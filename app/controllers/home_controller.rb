aac3dfe4ec08d6453ea9b3edfa6997d19dcb439f
Eric-Guo <eric.guocz@gmail.com>
2019-03-08 12:07:22 +0800

    Handle turbolink cause in openid

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
   before_action :doorkeeper_authorize!, only: :me
   before_action -> { doorkeeper_authorize! :public }, only: :profiles
+  content_security_policy_report_only only: :options_authorize
+  skip_before_action :verify_authenticity_token, only: :options_authorize
 
   def index
   end
@@ -11,5 +13,9 @@ class HomeController < ApplicationController
 
   def profiles
     render json: { info: 'more information here' }
+  end
+
+  def options_authorize
+    head :no_content
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   use_doorkeeper
   use_doorkeeper_openid_connect
+  match '/oauth/authorize' => 'home#options_authorize', via: :options
   devise_for :users, controllers: { sessions: 'user/sessions',
                                     passwords: 'user/passwords',
                                     confirmations: 'user/confirmations',
