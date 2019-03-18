c19c2a88148585e1242a63ec5d80258f16a771dd
Eric-Guo <eric.guocz@gmail.com>
2019-03-18 13:16:24 +0800

    Introduce the pundit

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ gem 'jbuilder', '~> 2.5'
 
 gem 'devise'
 gem 'doorkeeper-openid_connect'
+gem 'pundit'
 
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
---
+++ app/policies/user_policy.rb
@@ -0,0 +1,30 @@
+class UserPolicy < ApplicationPolicy
+  class Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user  = user
+      @scope = scope
+    end
+
+    def resolve
+      if user.admin?
+        scope.all
+      else
+        scope.none
+      end
+    end
+  end
+
+  def index?
+    user.admin?
+  end
+
+  def system_role_name
+    return unless user.present?
+
+    if user.admin?
+      ActionController::Base.helpers.content_tag :p, I18n.t('ui.system_administrator'), class: 'app-sidebar__user-designation'
+    end
+  end
+end
