b7e2f06431363c220e629bd7ea6f8427d7a1718c
Eric-Guo <eric.guocz@gmail.com>
2019-03-18 17:26:18 +0800

    Display application list in home page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @applications = Doorkeeper::Application.all
   end
 end
--- test/system/home_dashboards_test.rb
+++ test/system/home_dashboards_test.rb
@@ -4,7 +4,7 @@ class HomeDashboardsTest < ApplicationSystemTestCase
   test 'visiting the home dashboard' do
     visit root_url
 
-    assert_selector 'h1', text: I18n.t('ui.dashboard')
+    assert_selector 'h1', text: I18n.t('ui.my_application_list')
     assert_selector 'a.btn-dark', text: I18n.t('user.sign_in')
   end
 end
