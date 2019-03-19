45cf29f7b3f53ee96bad441c92ee8b455d747246
Eric-Guo <eric.guocz@gmail.com>
2019-03-19 09:12:42 +0800

    Also check the employee list being checked.

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,9 +1,11 @@
 class EmployeesController < ApplicationController
   before_action :authenticate_user!
-  after_action :verify_authorized, except: :index
+  after_action :verify_authorized
   after_action :verify_policy_scoped, only: :index
 
   def index
+    authorize User
+
     users = policy_scope(User)
     respond_to do |format|
       format.html
