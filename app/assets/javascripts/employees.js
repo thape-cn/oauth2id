f7df712efe9dc69bc8e45a4055cb83d883a7643d
Eric-Guo <eric.guocz@gmail.com>
2019-03-22 22:36:01 +0800

    Fix back and forward data table bug under turbolinks.

--- app/assets/javascripts/employees.js
+++ app/assets/javascripts/employees.js
@@ -1,15 +1,24 @@
 document.addEventListener("turbolinks:load", function() {
   "use strict";
 
-  $('#users-datatable').dataTable({
-    "processing": true,
-    "serverSide": true,
-    "ajax": $('#users-datatable').data('source'),
-    "pagingType": "full_numbers",
-    "columns": [
-      {"data": "id"},
-      {"data": "username"},
-      {"data": "email"}
-    ]
-  });
+  if ($("#users-datatable_wrapper").length == 0) {
+    $('#users-datatable').dataTable({
+      "processing": true,
+      "serverSide": true,
+      "ajax": $('#users-datatable').data('source'),
+      "pagingType": "full_numbers",
+      "columns": [
+        {"data": "id"},
+        {"data": "username"},
+        {"data": "email"}
+      ],
+      stateSave: true,
+      stateSaveCallback: function(settings,data) {
+          localStorage.setItem('DataTables_users-datatable', JSON.stringify(data));
+        },
+      stateLoadCallback: function(settings) {
+        return JSON.parse(localStorage.getItem('DataTables_users-datatable'));
+        }
+    });
+  }
 })
