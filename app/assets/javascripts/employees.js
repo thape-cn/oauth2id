985c5b6bf3ee211ab6c506cc9c13d6af79bff4be
Eric-Guo <eric.guocz@gmail.com>
2019-03-23 18:02:55 +0800

    Fix reloading problem by destroy & recreate.
    https://datatables.net/forums/discussion/comment/98080/#Comment_98080

--- app/assets/javascripts/employees.js
+++ app/assets/javascripts/employees.js
@@ -1,24 +1,28 @@
 document.addEventListener("turbolinks:load", function() {
   "use strict";
 
-  if ($("#users-datatable_wrapper").length == 0) {
-    $('#users-datatable').dataTable({
-      "processing": true,
-      "serverSide": true,
-      "ajax": $('#users-datatable').data('source'),
-      "pagingType": "full_numbers",
-      "columns": [
-        {"data": "id"},
-        {"data": "username"},
-        {"data": "email"}
-      ],
-      stateSave: true,
-      stateSaveCallback: function(settings,data) {
-          localStorage.setItem('DataTables_users-datatable', JSON.stringify(data));
-        },
-      stateLoadCallback: function(settings) {
-        return JSON.parse(localStorage.getItem('DataTables_users-datatable'));
-        }
-    });
+  $('#users-datatable').dataTable({
+    "processing": true,
+    "serverSide": true,
+    "ajax": $('#users-datatable').data('source'),
+    "pagingType": "full_numbers",
+    "columns": [
+      {"data": "id"},
+      {"data": "username"},
+      {"data": "email"}
+    ],
+    stateSave: true,
+    stateSaveCallback: function(settings, data) {
+        localStorage.setItem('DataTables_users-datatable', JSON.stringify(data));
+      },
+    stateLoadCallback: function(settings) {
+      return JSON.parse(localStorage.getItem('DataTables_users-datatable'));
+      }
+  });
+})
+
+document.addEventListener("turbolinks:before-cache", function() {
+  if($("#users-datatable_wrapper").length == 1) {
+    $('#users-datatable').DataTable().destroy();
   }
 })
