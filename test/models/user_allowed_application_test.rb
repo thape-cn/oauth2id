16625cebc9015891c9c89185356ca8854024c26c
Eric-Guo <eric.guocz@gmail.com>
2019-04-02 11:12:42 +0800

    Add test support for YunXueTang

--- test/models/user_allowed_application_test.rb
+++ test/models/user_allowed_application_test.rb
@@ -4,12 +4,12 @@ class UserAllowedApplicationTest < ActiveSupport::TestCase
   test "Eric's user_allowed_applications valid" do
     eric = users(:user_eric)
     assert eric.user_allowed_applications.all(&:valid?)
-    assert_equal eric.user_allowed_applications.count, 3
+    assert_equal eric.user_allowed_applications.count, 4
   end
 
   test "oauth_app_test's user_allowed_applications valid" do
     oauth_app_test = oauth_applications(:oauth_app_test)
     assert oauth_app_test.user_allowed_applications.all(&:valid?)
-    assert_equal oauth_app_test.user_allowed_applications.count, 2
+    assert_equal oauth_app_test.user_allowed_applications.count, 3
   end
 end
