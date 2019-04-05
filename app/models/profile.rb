0e369ae6887a5cb23d9658b90f26d7fe662a485d
Eric-Guo <eric.guocz@gmail.com>
2019-04-05 10:33:22 +0800

    Display profile field and allow profile field to be extendable.

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,8 @@
 class Profile < ApplicationRecord
   belongs_to :user
+  HIDDEN_ATTRIBUTES = %w[id user_id created_at updated_at].freeze
+
+  def custom_attributes
+    attributes.reject { |k,v| k.in? HIDDEN_ATTRIBUTES }
+  end
 end
