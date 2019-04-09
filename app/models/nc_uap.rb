77c750839c763d1001cf038748d568ef3d932625
Eric-Guo <eric.guocz@gmail.com>
2019-04-09 10:33:34 +0800

    Sync job level, sex and begin date from NC UAP

--- app/models/nc_uap.rb
+++ app/models/nc_uap.rb
@@ -8,11 +8,12 @@ class NcUap < ApplicationRecord
 
   def self.nc_users
     NcUap.connection.select_rows("
-select convert(bd_psndoc.name,'ZHS16GBK', 'UTF8') name, hi_psnjob.clerkcode, bd_psndoc.email,
-       hi_psnjob.pk_dept, hi_psnjob.pk_post, om_joblevel.name
+select convert(bd_psndoc.name,'ZHS16GBK', 'UTF8') name, bd_psndoc.SEX, hi_psnjob.clerkcode, bd_psndoc.email,
+       hi_psnjob.pk_dept, hi_psnjob.pk_post, om_joblevel.name, bd_psndoc.birthdate, hi_psnorg.begindate
 from bd_psndoc
 inner join hi_psnjob on bd_psndoc.pk_psndoc = hi_psnjob.pk_psndoc
 left join om_joblevel on om_joblevel.pk_joblevel = hi_psnjob.pk_jobgrade
+left join hi_psnorg on hi_psnorg.pk_psndoc = bd_psndoc.pk_psndoc
 where hi_psnjob.ismainjob = 'Y'
   and hi_psnjob.lastflag = 'Y'
   and hi_psnjob.endflag = 'N'
@@ -21,6 +22,7 @@ where hi_psnjob.ismainjob = 'Y'
   and hi_psnjob.clerkcode not in ('002541','012096')
   and bd_psndoc.email not in ('yangxiao@thape.com.cn ')
   and bd_psndoc.email not in ('xieyong@','xiezhipeng@','**','#n/a','#N/A','#','##','###','####','#####','####cn','*','/','0','111','1111','123','123456','213412341234','6699','=')
+
 ")
   end
 
@@ -28,10 +30,14 @@ where hi_psnjob.ismainjob = 'Y'
     users = NcUap.nc_users
     users.each do |u|
       chinese_name = u[0]
-      clerk_code = u[1]
-      email = u[2]
-      pk_dept = u[3]
-      pk_post = u[4]
+      sex = u[1]
+      clerk_code = u[2]
+      email = u[3]
+      pk_dept = u[4]
+      pk_post = u[5]
+      job_level = u[6]
+      birthdate = u[7]
+      entry_company_date = u[8]
       puts "Import user: #{chinese_name}"
       user = User.find_or_create_by!(email: email.downcase) do |user|
         user.username ||= email.split('@').first == '#' ? clerk_code : email.split('@').first
@@ -40,7 +46,11 @@ where hi_psnjob.ismainjob = 'Y'
       end
       profile = user.profile || user.build_profile
       profile.chinese_name = chinese_name
+      profile.gender = (sex == 2 ? 0 : sex) # Femail is 0 in oauth2id
       profile.clerk_code = clerk_code
+      profile.job_level = job_level
+      profile.birthdate = birthdate
+      profile.entry_company_date = entry_company_date
       profile.save
 
       user_department = Department.find_by(nc_pk_dept: pk_dept)
