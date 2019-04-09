63fbad58de8353570e20f234b3a455bc804a7559
Eric-Guo <eric.guocz@gmail.com>
2019-04-09 10:33:34 +0800

    Sync job level, sex and begin date from NC UAP

--- db/migrate/20190404060713_add_more_field_to_profile.rb
+++ db/migrate/20190404060713_add_more_field_to_profile.rb
@@ -2,5 +2,8 @@ class AddMoreFieldToProfile < ActiveRecord::Migration[5.2]
   def change
     add_column :profiles, :clerk_code, :string
     add_column :profiles, :chinese_name, :string
+    add_column :profiles, :job_level, :string
+    add_column :profiles, :birthdate, :date
+    add_column :profiles, :entry_company_date, :date
   end
 end
