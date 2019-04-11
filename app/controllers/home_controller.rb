ebf5e950301358fdfac10b00aeb0db56d2fd48f7
Eric-Guo <eric.guocz@gmail.com>
2019-04-11 11:02:00 +0800

    Display in dashbaord for the allow_login_by_default applications.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 class HomeController < ApplicationController
   def index
     @applications = if current_user.present?
-      current_user.user_allowed_applications.where(enable: true).collect(&:oauth_application)
+      user_allowed_application_ids = current_user.user_allowed_applications.where(enable: true).pluck(:oauth_application_id)
+      Doorkeeper::Application.where(id: user_allowed_application_ids)
+        .or(Doorkeeper::Application.where(allow_login_by_default: true))
     else
       Doorkeeper::Application.all
     end
