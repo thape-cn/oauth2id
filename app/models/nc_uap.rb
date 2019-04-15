94b7b0dbec65fcce3b5de7f2827f9201d2e42cf9
Eric-Guo <eric.guocz@gmail.com>
2019-04-15 14:56:19 +0800

    Reuse the positions

--- app/models/nc_uap.rb
+++ app/models/nc_uap.rb
@@ -63,7 +63,11 @@ where hi_psnjob.ismainjob = 'Y'
 
       if pk_post != '~'
         user_position = Position.find_by(nc_pk_post: pk_post)
-        PositionUser.find_or_create_by!(user_id: user.id, position_id: user_position.id, main_position: true)
+        user_position_company_name = user_position.department.company_name
+        real_position = Position.find_or_create_by!(name: user_position.name,
+          functional_category: user_position.functional_category,
+          nc_pk_post: nil, department_id: nil, company_name: user_position_company_name)
+        PositionUser.find_or_create_by!(user_id: user.id, position_id: real_position.id, main_position: true)
       end
     end
   end
