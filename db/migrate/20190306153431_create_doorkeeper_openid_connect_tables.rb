1c718fe99a11b56e818b1aca25dac90be306da44
Eric-Guo <eric.guocz@gmail.com>
2019-04-16 09:49:48 +0800

    Fix MySQL deploy problem.

--- db/migrate/20190306153431_create_doorkeeper_openid_connect_tables.rb
+++ db/migrate/20190306153431_create_doorkeeper_openid_connect_tables.rb
@@ -1,7 +1,16 @@
 class CreateDoorkeeperOpenidConnectTables < ActiveRecord::Migration[5.2]
+  def mysql?
+    config = ActiveRecord::Base.configurations[Rails.env]
+    config && config['adapter'] == 'mysql2'
+  end
+
   def change
     create_table :oauth_openid_requests do |t|
-      t.integer :access_grant_id, null: false
+      if mysql?
+        t.bigint :access_grant_id, null: false
+      else
+        t.integer :access_grant_id, null: false
+      end
       t.string :nonce, null: false
     end
 
