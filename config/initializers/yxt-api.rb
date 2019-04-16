3857da1d20e82b4d8e447fd2bf1ce2b7363b3792
Eric-Guo <eric.guocz@gmail.com>
2019-04-16 13:54:35 +0800

    Using production API host of yxt.

--- config/initializers/yxt-api.rb
+++ config/initializers/yxt-api.rb
@@ -1,3 +1,3 @@
 Yxt.apikey     = Rails.application.credentials.yxt_apikey!
 Yxt.secretkey  = Rails.application.credentials.yxt_secretkey!
-Yxt.base_url  = 'http://api-netadel.yunxuetang.com.cn'
+Yxt.base_url  = 'http://api.yunxuetang.cn'
