61730d03adf4cba274cc513aecc141ff535c29d1
Eric-Guo <eric.guocz@gmail.com>
2019-05-05 17:20:32 +0800

    Display Chinese name and clerk code as well.

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -17,7 +17,7 @@ class EmployeesController < ApplicationController
                                 .references(:department_users)
             else
               policy_scope(User)
-            end
+            end.left_joins(:profile).references(:profile)
     respond_to do |format|
       format.html
       format.json { render json: UserDatatable.new(params, users: users, view_context: view_context) }
@@ -31,11 +31,13 @@ class EmployeesController < ApplicationController
       format.csv do
         render_csv_header :user_report.to_s
         csv_res = CSV.generate do |csv|
-          csv << ['ID', 'User Name', 'eMail', 'Title', 'Department', 'Position']
+          csv << ['ID', 'User Name', 'Chinese Name', 'Clerk Code', 'eMail', 'Title', 'Department', 'Position']
           policy_scope(User).order(id: :asc).find_each do |user|
             values = []
             values << user.id
             values << user.username
+            values << user.profile.chinese_name
+            values << user.profile.clerk_code
             values << user.email
             values << user.profile&.title
             values << user.departments.pluck(:name).join(';')
