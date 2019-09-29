87951bf14b6cffe24fe16792242cd85a2d61653c
Eric-Guo <eric.guocz@gmail.com>
2019-09-29 09:10:43 +0800

    Disable the login if javascript is not loading properly.

--- app/assets/javascripts/devise-jwt.js
+++ app/assets/javascripts/devise-jwt.js
@@ -3,4 +3,6 @@ document.addEventListener("turbolinks:load", function() {
     const xhr = event.detail[0];
     xhr.setRequestHeader('JWT-AUD', 'oauth2id');
   });
+  $("#user-sign-in-btn").prop('disabled', false).removeClass('disabled');
+  $("#user-sign-in-hint").hide();
 });
