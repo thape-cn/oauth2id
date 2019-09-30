3f8e2fa6712605090f8d7c67a492af206b4a6f14
Eric-Guo <eric.guocz@gmail.com>
2019-09-30 15:15:21 +0800

    Show login errors.

--- app/assets/javascripts/devise-jwt.js
+++ app/assets/javascripts/devise-jwt.js
@@ -3,6 +3,16 @@ document.addEventListener("turbolinks:load", function() {
     const xhr = event.detail[0];
     xhr.setRequestHeader('JWT-AUD', 'oauth2id');
   });
+  $("#user-sign-in").on("ajax:error", function(event) {
+    const detail = event.originalEvent.detail;
+    $.notify({
+      title: detail[1],
+      message: detail[0],
+      icon: 'fa fa-alert'
+    },{
+      type: "warning"
+    });
+  });
   $("#user-sign-in-btn").prop('disabled', false).removeClass('disabled');
   $("#user-sign-in-hint").hide();
 });
--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -7,8 +7,8 @@ class User::SessionsController < Devise::SessionsController
   def create
     super do |user|
       user.user_sign_in_histories
-        .create(sign_in_at: Time.current,
-                user_agent: request.user_agent, sign_in_ip: request.remote_ip)
+          .create(sign_in_at: Time.current,
+                  user_agent: request.user_agent, sign_in_ip: request.remote_ip)
     end
   end
 end
