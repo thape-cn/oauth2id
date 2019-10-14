66e6c9e4adfee8e7e80b641da0747db1622ebf10
Eric-Guo <eric.guocz@gmail.com>
2019-10-14 10:27:58 +0800

    If user key in @thape.com.cn, remove it and continue login with his real name.

--- app/assets/javascripts/devise-jwt.js
+++ app/assets/javascripts/devise-jwt.js
@@ -1,8 +1,18 @@
 document.addEventListener("turbolinks:load", function() {
+  $("#user-sign-in").on("ajax:before", function(event, settings) {
+    let user_name = event.currentTarget[2].value
+    let user_array = user_name.split('@');
+    if(user_array.length > 1 && user_array[1]=='thape.com.cn') {
+      event.currentTarget[2].value = user_array[0];
+    }
+    return true;
+  });
+
   $("#user-sign-in").on("ajax:beforeSend", function(event, settings) {
     const xhr = event.detail[0];
     xhr.setRequestHeader('JWT-AUD', 'oauth2id');
   });
+
   $("#user-sign-in").on("ajax:error", function(event) {
     const detail = event.originalEvent.detail;
     $.notify({
