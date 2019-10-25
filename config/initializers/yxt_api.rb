89c5fc0279ad97b9f991c52c420e60fc7220bd64
Eric-Guo <eric.guocz@gmail.com>
2019-10-25 13:40:37 +0800

    Change YXT dev server API server from .NET to Java

--- config/initializers/yxt_api.rb
+++ config/initializers/yxt_api.rb
@@ -1,9 +1,9 @@
 if Rails.env.development?
   Yxt.apikey     = Rails.application.credentials.yxt_test_apikey!
   Yxt.secretkey  = Rails.application.credentials.yxt_test_secretkey!
-  Yxt.base_url  = 'http://api-netadel.yunxuetang.com.cn'
+  Yxt.base_url = 'http://api-udp-adel.yunxuetang.com.cn'
 else
   Yxt.apikey     = Rails.application.credentials.yxt_apikey!
   Yxt.secretkey  = Rails.application.credentials.yxt_secretkey!
-  Yxt.base_url  = 'https://apic1.yunxuetang.cn'
+  Yxt.base_url = 'https://apic1.yunxuetang.cn'
 end
