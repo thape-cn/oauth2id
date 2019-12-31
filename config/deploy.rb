4f8cab434279dde8a293c32e5d329f8b5056f0e7
Eric-Guo <eric.guocz@gmail.com>
2019-12-31 11:34:30 +0800

    change git source server

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '~> 3.16.0'
 
 set :application, 'oauth2id'
-set :repo_url, 'git@git.coding.net:ericguo/oauth2id.git'
+set :repo_url, 'git@git.dev.tencent.com:ericguo/oauth2id.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
