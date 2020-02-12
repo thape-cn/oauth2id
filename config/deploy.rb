aa48d52a8f96f617866d811c2edbd3a5fb3d7db1
Eric-Guo <eric.guocz@gmail.com>
2020-02-12 09:59:47 +0800

    upgrade gems

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock '~> 3.11.2'
+lock '~> 3.12.0'
 
 set :application, 'oauth2id'
 set :repo_url, 'git@github.com:thape-cn/oauth2id.git'
