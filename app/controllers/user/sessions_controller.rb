572a5c74ea2c0e8351e5df1473d75ce994e3bcb8
Eric-Guo <eric.guocz@gmail.com>
2020-03-17 10:44:31 +0800

    New fields for sync with cybros.

--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -10,10 +10,32 @@ class User::SessionsController < Devise::SessionsController
           .create(sign_in_at: Time.current,
                   user_agent: request.user_agent, sign_in_ip: request.remote_ip)
       whitelisted_jwts_attributes = user.whitelisted_jwts.last.attributes.except('id', 'user_id')
-      sync_white_jwts_attrs = whitelisted_jwts_attributes.merge(email: user.email)
 
-      Rails.logger.debug "sync_white_jwts_attrs: #{sync_white_jwts_attrs}"
-      response = HTTP.options(Rails.application.credentials.sync_white_jwts_url!, json: sync_white_jwts_attrs)
+      main_position = user.position_users.find_by(main_position: true)&.position
+      main_position = user.position_users.last&.position if main_position.nil?
+
+      combine_deparments = user.departments.collect do |department|
+        {
+          id: department.id,
+          name: department.name,
+          dept_code: department.dept_code,
+          company_name: department.company_name,
+          company_code: department.company_code
+        }
+      end
+
+      user_attrs = {
+        white_jwts_attrs: whitelisted_jwts_attributes,
+        email: user.email,
+        position_title: main_position&.name,
+        clerk_code: user.profile&.clerk_code,
+        chinese_name: user.profile&.chinese_name,
+        job_level: user.profile&.job_level,
+        desk_phone: user.desk_phone,
+        departments: combine_deparments
+      }
+      Rails.logger.debug "user_attrs: #{user_attrs}"
+      response = HTTP.options(Rails.application.credentials.sync_white_jwts_url!, json: user_attrs)
       Rails.logger.debug response
     end
   rescue Exception => e
