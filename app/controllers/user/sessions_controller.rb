ff274e3436c76ace8a5a2370b517bd2b136895b2
Eric-Guo <eric.guocz@gmail.com>
2020-08-20 14:13:54 +0800

    Don't call devise default controller, extract logic to own.

--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -7,42 +7,46 @@ class User::SessionsController < Devise::SessionsController
   after_action :cors_set_access_control_headers, only: [:create]
 
   def create
-    super do |user|
-      user.user_sign_in_histories
-          .create(sign_in_at: Time.current,
-                  user_agent: request.user_agent, sign_in_ip: request.remote_ip)
-      allowlisted_jwts_attributes = user.allowlisted_jwts.last.attributes.except('id', 'user_id')
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message!(:notice, :signed_in)
+    sign_in(resource_name, resource)
 
-      main_position = user.position_users.find_by(main_position: true)&.position
-      main_position = user.position_users.last&.position if main_position.nil?
+    self.resource.user_sign_in_histories
+        .create(sign_in_at: Time.current,
+                user_agent: request.user_agent, sign_in_ip: request.remote_ip)
+    allowlisted_jwts_attributes = self.resource.allowlisted_jwts.last.attributes.except('id', 'user_id')
 
-      combine_deparments = user.departments.collect do |department|
-        {
-          id: department.id,
-          name: department.name,
-          dept_code: department.dept_code,
-          company_name: department.company_name,
-          company_code: department.company_code
-        }
-      end
+    main_position = self.resource.position_users.find_by(main_position: true)&.position
+    main_position = self.resource.position_users.last&.position if main_position.nil?
 
-      user_attrs = {
-        white_jwts_attrs: allowlisted_jwts_attributes,
-        email: user.email,
-        position_title: main_position&.name,
-        clerk_code: user.profile&.clerk_code,
-        chinese_name: user.profile&.chinese_name,
-        job_level: user.profile&.job_level,
-        locked_at: user.locked_at&.to_date,
-        desk_phone: user.desk_phone,
-        departments: combine_deparments
+    combine_deparments = self.resource.departments.collect do |department|
+      {
+        id: department.id,
+        name: department.name,
+        dept_code: department.dept_code,
+        company_name: department.company_name,
+        company_code: department.company_code
       }
-      Rails.logger.debug "user_attrs: #{user_attrs}"
-      response1 = HTTP.options(Rails.application.credentials.sync_white_jwts_url1!, json: user_attrs)
-      Rails.logger.debug response1
-      response2 = HTTP.options(Rails.application.credentials.sync_white_jwts_url2!, json: user_attrs)
-      Rails.logger.debug response2
     end
+
+    user_attrs = {
+      white_jwts_attrs: allowlisted_jwts_attributes,
+      email: self.resource.email,
+      position_title: main_position&.name,
+      clerk_code: self.resource.profile&.clerk_code,
+      chinese_name: self.resource.profile&.chinese_name,
+      job_level: self.resource.profile&.job_level,
+      locked_at: self.resource.locked_at&.to_date,
+      desk_phone: self.resource.desk_phone,
+      departments: combine_deparments
+    }
+    Rails.logger.debug "user_attrs: #{user_attrs}"
+    response1 = HTTP.options(Rails.application.credentials.sync_white_jwts_url1!, json: user_attrs)
+    Rails.logger.debug response1
+    response2 = HTTP.options(Rails.application.credentials.sync_white_jwts_url2!, json: user_attrs)
+    Rails.logger.debug response2
+
+    respond_with resource, location: after_sign_in_path_for(resource)
   rescue Exception => e
     Rails.logger.debug e
     redirect_to new_user_session_path, alert: "Error creating session"
