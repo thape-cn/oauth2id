9af1a393159a0f1dd901290c59a3300c31f841b7
Eric-Guo <eric.guocz@gmail.com>
2021-03-30 18:09:20 +0800

    The clerk_code must existing.

--- app/controllers/duplicate_employees_controller.rb
+++ app/controllers/duplicate_employees_controller.rb
@@ -6,6 +6,7 @@ class DuplicateEmployeesController < ApplicationController
       .select('chinese_name, clerk_code, COUNT(user_id) account_counts')
       .group(:chinese_name, :clerk_code)
       .having('count(user_id) > 1')
+      .where.not(clerk_code: nil)
       .order('account_counts DESC, clerk_code DESC')
   end
 
