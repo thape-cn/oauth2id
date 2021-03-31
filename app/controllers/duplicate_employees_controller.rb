836de3d287e7491aae50fdfc34054a523cc8a2f9
Eric-Guo <eric.guocz@gmail.com>
2021-03-31 14:24:13 +0800

    Allow merge user.

--- app/controllers/duplicate_employees_controller.rb
+++ app/controllers/duplicate_employees_controller.rb
@@ -16,5 +16,25 @@ class DuplicateEmployeesController < ApplicationController
   end
 
   def update
+    main_user = User.find params[:main_user_id]
+    to_remove_users = User.joins(:profile)
+      .where(profile: { clerk_code: main_user.profile.clerk_code })
+      .where.not(id: main_user.id)
+
+    to_remove_users.each do |merge_user|
+      merge_user.department_users.pluck(:department_id).each do |department_id|
+        main_user.department_users.find_or_create_by(department_id: department_id)
+      end
+      merge_user.position_users.pluck(:position_id).each do |position_id|
+        main_user.position_users.find_or_create_by(position_id: position_id)
+      end
+      UserSignInHistory.where(user_id: merge_user.id).update_all(user_id: main_user.id)
+      merge_user.user_allowed_applications.pluck(:oauth_application_id).each do |oauth_application_id|
+        main_user.user_allowed_applications.find_or_create_by(oauth_application_id: oauth_application_id)
+      end
+      merge_user.destroy!
+    end
+
+    redirect_to duplicate_employee_path(id: main_user.profile.clerk_code), notice: I18n.t('ui.merge_success')
   end
 end
