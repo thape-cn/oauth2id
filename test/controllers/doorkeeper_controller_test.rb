65f89f514fc29658a1b5633177d8f56679d9f112
Eric-Guo <eric.guocz@gmail.com>
2021-05-26 10:23:17 +0800

    Fix test.

--- test/controllers/doorkeeper_controller_test.rb
+++ test/controllers/doorkeeper_controller_test.rb
@@ -2,10 +2,11 @@ require 'test_helper'
 
 class DoorkeeperControllerTest < ActionDispatch::IntegrationTest
   test 'should get token' do
-    post oauth_token_url, params: { email: 'eric@cloud-mes.com',
-                                    password: '123456',
-                                    grant_type: 'password' }
-    assert_response :success
-    assert_match 'Bearer', JSON.parse(@response.body)['token_type']
+    post user_session_path, as: :json,
+                            params: { user: { email: 'eric@cloud-mes.com',
+                                              password: '123456' } },
+                            headers: { 'HTTP_JWT_AUD': 'oauth2id_test' }
+    assert_response :created
+    assert_match 'Bearer', @response.header['Authorization']
   end
 end
