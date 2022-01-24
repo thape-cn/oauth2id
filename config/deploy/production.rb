442117aaf5de6751e19aec6489146b19405a1c45
Eric-Guo <eric.guocz@gmail.com>
2022-01-24 16:13:20 +0800

    Add one additional SSO server.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,8 @@
 set :nginx_use_ssl, true
+set :rails_env, 'production'
 set :branch, :thape
+set :puma_service_unit_name, :puma_oauth2id_production
 set :puma_systemctl_user, :system
 
 server 'thape_sso', user: 'deployer', roles: %w{app db web}
+server 'thape_gitlab', user: 'deployer', roles: %w{app web}
