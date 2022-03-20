5c02b1a7f6c67ae513855ad5801ae7fbafc128fc
Eric-Guo <eric.guocz@gmail.com>
2022-03-20 22:17:31 +0800

    gem and yarn upgrade

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock '~> 3.16.0'
+lock '~> 3.17.0'
 
 set :application, 'oauth2id'
 set :repo_url, 'git@github.com:thape-cn/oauth2id.git'
