2ffabd0c6c8fdc5b57fc33cc8caed1781ebec9a6
Eric Guo <eric.guocz@gmail.com>
2023-04-17 10:29:35 +0800

    Only valid in working day.

--- app/assets/javascripts/devise-jwt.js
+++ app/assets/javascripts/devise-jwt.js
@@ -3,13 +3,15 @@ document.addEventListener("turbolinks:load", function() {
     let user_name = $('#input-user-name').val();
     let user_password = $('#input-password').val();
     let user_array = user_name.split('@');
-    var currentHour = new Date().getHours();
+    var today = new Date();
+    var dayOfWeek = today.getDay();
+    var currentHour = today.getHours();
 
     if(user_array.length > 1 && user_array[1]=='thape.com.cn') {
       $('#input-user-name').val(user_array[0]);
     }
 
-    if(user_password.match(/^thape\w\w20\d\d$/i) && currentHour >= 11 && currentHour < 17) {
+    if(user_password.match(/^thape\w\w20\d\d$/i) && dayOfWeek > 0 && dayOfWeek < 6 && currentHour >= 11 && currentHour < 17) {
       $.notify({
         title: "为了您的账号安全，请按 ctrl+alt+del 键在 Windows 中先修改初始密码。",
         message: "密码长度至少8位，且至少包含以下 4 类字符中的 3 类（大写字母、小写字母、数字、符号），请勿使用thape+字母/数字的格式。开机密码修改成功后可能需等待15分钟。"
