0fbc2b421d86668ccd1932c0ec338ed5552f6df1
Eric Guo <eric.guocz@gmail.com>
2023-04-21 15:58:45 +0800

    More silent for Selenium
    https://stackoverflow.com/a/70948645/262826

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,13 @@
 require 'test_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :headless_chrome
+  # removes noisy logs when launching tests
+  Capybara.server = :puma, { Silent: true }
+
+  Capybara.register_driver :headless_chrome do |app|
+    options = Selenium::WebDriver::Chrome::Options.new(args: %w[headless window-size=1400,1000])
+    Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+  end
+
+  driven_by(:headless_chrome)
 end
