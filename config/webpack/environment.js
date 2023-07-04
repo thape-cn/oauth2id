900cde74b2acc891c5a0a171c9415ebf4609e7d9
Eric Guo <eric.guocz@gmail.com>
2023-07-04 14:07:45 +0800

    most elegant solution
    https://stackoverflow.com/a/72219174/262826

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,19 @@
 const { environment } = require('@rails/webpacker')
 
+const crypto = require('crypto');
+
+/**
+ * The MD4 algorithm is not available anymore in Node.js 17+ (because of library SSL 3).
+ * In that case, silently replace MD4 by the MD5 algorithm.
+ */
+try {
+  crypto.createHash('md4');
+} catch (e) {
+  console.warn('Crypto "MD4" is not supported anymore by this Node.js version');
+  const origCreateHash = crypto.createHash;
+  crypto.createHash = (alg, opts) => {
+    return origCreateHash(alg === 'md4' ? 'md5' : alg, opts);
+  };
+}
+
 module.exports = environment
