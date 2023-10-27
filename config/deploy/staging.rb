cf2279f50acee386e3d67690f575fbeca5f3b9bc
Eric Guo <eric.guocz@gmail.com>
2023-10-27 16:19:45 +0800

    Another missing.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,6 @@
 set :nginx_use_ssl, true
 set :branch, :main
+set :puma_service_unit_name, :puma_oauth2id_staging
 set :puma_systemctl_user, :system
 
 server 'ericsg', user: 'ec2-user', roles: %w{app db web}
