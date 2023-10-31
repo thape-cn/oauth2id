aa057413e48d0c64af5c6b83341e8452a8ed677d
Eric Guo <eric.guocz@gmail.com>
2023-10-31 13:15:44 +0800

    Revert "Fallback to Rails 7.0.8"

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2021_03_26_082831) do
+ActiveRecord::Schema[7.1].define(version: 2021_03_26_082831) do
   create_table "allowlisted_jwts", force: :cascade do |t|
     t.string "jti", null: false
     t.string "aud", null: false
