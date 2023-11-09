6061b85c4c952e7c094472f32ebc12a776fac6bc
Eric-Guo <eric.guocz@gmail.com>
2023-11-09 21:54:34 +0800

    GPT4, I want to make new sign up user as admin if such user is the first user, how to modify the code?

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -1,6 +1,16 @@
 class User::RegistrationsController < Devise::RegistrationsController
   layout 'sessions', only: %i[new create]
 
+  def create
+    super do |user|
+      if User.count == 1
+        user.admin = true
+        user.save
+        user.confirm
+      end
+    end
+  end
+
   private
 
   def sign_up_params
