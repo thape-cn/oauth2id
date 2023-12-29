bf013726b6b220a7a97accf666a42aa212f6c220
Eric Guo <eric.guocz@gmail.com>
2023-12-29 14:36:37 +0800

    Turn it off in thape branch.

--- test/controllers/user/registrations_controller_test.rb
+++ test/controllers/user/registrations_controller_test.rb
@@ -4,8 +4,8 @@ class RegistrationsControllerTest < ActionDispatch::IntegrationTest
     User.destroy_all
   end
 
-  test 'should register a new user' do
-    assert_difference -> { User.count }, 1 do
+  test 'Can not register a new user' do
+    assert_difference -> { User.count }, 0 do
       post user_registration_path, params: { user: { username: 'firstuser', email: 'firstuser@example.com', password: '12345678aa', password_confirmation: '12345678aa' } }
       assert_response :redirect
     end
