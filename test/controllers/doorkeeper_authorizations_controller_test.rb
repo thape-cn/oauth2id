bd39c43dba2a0b0eb38bccab18cb70a19a5119ce
Eric Guo <eric.guocz@gmail.com>
2024-01-06 19:07:17 +0800

    Add test to test disallowed_user access allow login by default application

--- test/controllers/doorkeeper_authorizations_controller_test.rb
+++ test/controllers/doorkeeper_authorizations_controller_test.rb
@@ -48,4 +48,18 @@ class DoorkeeperAuthorizationsControllerTest < ActionDispatch::IntegrationTest
                                              scope: 'public' }
     end
   end
+
+  test 'should allow login if user is not allowed but application allow login by default' do
+    disallowed_user = users(:user_shin)
+    allow_login_by_default_app = oauth_applications(:oauth_app_test)
+
+    sign_in disallowed_user
+
+    get oauth_authorization_url, params: { client_id: allow_login_by_default_app.uid,
+                                           redirect_uri: allow_login_by_default_app.redirect_uri,
+                                           response_type: 'code',
+                                           scope: 'public' }
+    assert_response :success
+    assert_match I18n.t('doorkeeper.authorizations.new.title'), @response.body
+  end
 end
