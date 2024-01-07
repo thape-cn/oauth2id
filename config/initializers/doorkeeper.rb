5a01486af317cfde8433a5d58a951496b250219a
Eric Guo <eric.guocz@gmail.com>
2024-01-07 17:33:48 +0800

    No need go through very condition.

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -10,12 +10,12 @@ Doorkeeper.configure do
     client_id = routes.params[:client_id]
 
     if current_user
-      authenticate_application = DoorkeeperApplication.find_by!(uid: client_id)
-      user_allowed = authenticate_application.user_allowed_access?(current_user.id)
-      department_allowed = authenticate_application.department_allowed_access?(current_user.department_users.pluck(:department_id))
-      position_allowed = authenticate_application.position_allowed_access?(current_user.position_users.pluck(:position_id))
+      app = DoorkeeperApplication.find_by!(uid: client_id)
 
-      if user_allowed || department_allowed || position_allowed || authenticate_application.allow_login_by_default?
+      if app.allow_login_by_default? \
+        || app.user_allowed_access?(current_user.id) \
+        || app.department_allowed_access?(current_user.department_users.pluck(:department_id)) \
+        || app.position_allowed_access?(current_user.position_users.pluck(:position_id))
         current_user
       else
         raise Doorkeeper::Errors::DoorkeeperError.new(I18n.t('ui.can_not_grant_applications'))
