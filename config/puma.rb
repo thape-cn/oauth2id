f2826cb11dd98b0c1e696a461ee9ff49202831ec
Eric-Guo <eric.guocz@gmail.com>
2025-10-09 21:01:53 +0800

    Also listen on sockets.

--- config/puma.rb
+++ config/puma.rb
@@ -16,6 +16,7 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
 port ENV.fetch("PORT") { 3000 }
+bind "unix://tmp/sockets/puma.sock"
 
 # Specifies the `environment` that Puma will run in.
 #
