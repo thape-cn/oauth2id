9dda39a873b7f1bb18e64ba3f9e1691e8d6c37ad
Eric-Guo <eric.guocz@gmail.com>
2025-11-04 11:53:01 +0800

    Need login first

--- test/system/home_dashboards_test.rb
+++ test/system/home_dashboards_test.rb
@@ -2,9 +2,12 @@ require 'application_system_test_case'
 
 class HomeDashboardsTest < ApplicationSystemTestCase
   test 'visiting the home dashboard' do
+    user_eric = users(:user_eric)
+    login_as user_eric, scope: :user
     visit root_url
 
     assert_selector 'h1', text: I18n.t('ui.my_application_list')
-    assert_selector 'a.btn-dark', text: I18n.t('user.sign_in')
+    find('a.app-nav__item[aria-label="Open Profile Menu"]').click
+    assert_selector '.dropdown-item', text: I18n.t('ui.logout')
   end
 end
