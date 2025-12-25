064f953a43015661b882fab25adc055a76538cde
Eric-Guo <eric.guocz@gmail.com>
2025-12-25 21:25:28 +0800

    Return agent keys.

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -7,11 +7,29 @@ module API
     def user_info
       u = current_user
       profile = u.profile
-      render json: {
-        chinese_name: profile&.chinese_name || u.username,
-        clerk_code: profile&.clerk_code,
-        email: u.email
-      }
+      if u.user_allowed_applications.find_by(oauth_application_id: 60, enable: true)
+        render json: {
+          chinese_name: profile&.chinese_name || u.username,
+          clerk_code: profile&.clerk_code,
+          opencode_api_key: Rails.application.credentials.opencode_api_key,
+          kimi_api_key: Rails.application.credentials.kimi_api_key,
+          moonshot_api_key: Rails.application.credentials.moonshot_api_key,
+          exa_api_key: Rails.application.credentials.exa_api_key,
+          deepseek_api_key: Rails.application.credentials.deepseek_api_key,
+          email: u.email
+        }
+      else
+        render json: {
+          chinese_name: profile&.chinese_name || u.username,
+          clerk_code: 'No access',
+          opencode_api_key: nil,
+          kimi_api_key: nil,
+          moonshot_api_key: nil,
+          exa_api_key: nil,
+          deepseek_api_key: nil,
+          email: u.email
+        }
+      end
     end
   end
 end
