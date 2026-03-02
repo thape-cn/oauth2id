cc358bbc045f89ee32d87a2eead4d2e9d2ac455b
Eric-Guo <eric.guocz@gmail.com>
2026-03-02 11:42:29 +0800

    New dev keys.

--- config/initializers/yxt_api.rb
+++ config/initializers/yxt_api.rb
@@ -1,9 +1,10 @@
 if Rails.env.development?
-  Yxt.apikey     = Rails.application.credentials.yxt_test_apikey!
-  Yxt.secretkey  = Rails.application.credentials.yxt_test_secretkey!
-  Yxt.base_url = 'https://api-qidac1.yunxuetang.cn'
+  Yxt.app_id = Rails.application.credentials.yxt_test_app_id!
+  Yxt.app_secret = Rails.application.credentials.yxt_test_app_secret!
+  Yxt.base_url = 'https://openapi-adel.yunxuetang.cn'
+  Yxt.token_cache_file = '/tmp/yxt-access-token-your_app_id.json'
 else
-  Yxt.apikey     = Rails.application.credentials.yxt_apikey!
-  Yxt.secretkey  = Rails.application.credentials.yxt_secretkey!
-  Yxt.base_url = 'https://api-qidac1.yunxuetang.cn'
+  Yxt.app_id = Rails.application.credentials.yxt_apikey!
+  Yxt.app_secret = Rails.application.credentials.yxt_secretkey!
+  Yxt.base_url = 'https://openapi.yunxuetang.cn'
 end
