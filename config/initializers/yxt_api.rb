e893a2dfe6cf8db5a4b89d795a800c2cda77549e
Eric-Guo <eric.guocz@gmail.com>
2026-03-02 15:03:24 +0800

    Seem testing also need production URL.

--- config/initializers/yxt_api.rb
+++ config/initializers/yxt_api.rb
@@ -1,7 +1,7 @@
 if Rails.env.development?
   Yxt.app_id = Rails.application.credentials.yxt_test_app_id!
   Yxt.app_secret = Rails.application.credentials.yxt_test_app_secret!
-  Yxt.base_url = 'https://openapi-adel.yunxuetang.cn'
+  Yxt.base_url = 'https://openapi.yunxuetang.cn'
   Yxt.token_cache_file = '/tmp/yxt-access-token-your_app_id.json'
 else
   Yxt.app_id = Rails.application.credentials.yxt_apikey!
