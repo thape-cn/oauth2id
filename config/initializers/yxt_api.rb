f4be7062d8e4a95e963da0992808bbd2111b1797
Eric-Guo <eric.guocz@gmail.com>
2026-03-03 16:17:54 +0800

    Rename

--- config/initializers/yxt_api.rb
+++ config/initializers/yxt_api.rb
@@ -1,10 +1,10 @@
 if Rails.env.development?
   Yxt.app_id = Rails.application.credentials.yxt_test_app_id!
   Yxt.app_secret = Rails.application.credentials.yxt_test_app_secret!
-  Yxt.base_url = 'https://openapi.yunxuetang.cn'
+  Yxt.api_origin_url = 'https://openapi.yunxuetang.cn'
   Yxt.token_cache_file = '/tmp/yxt-access-token-your_app_id.json'
 else
   Yxt.app_id = Rails.application.credentials.yxt_apikey!
   Yxt.app_secret = Rails.application.credentials.yxt_secretkey!
-  Yxt.base_url = 'https://openapi.yunxuetang.cn'
+  Yxt.api_origin_url = 'https://openapi.yunxuetang.cn'
 end
